d752c314b56e00b5b088ac1722b2d44f2ca3c0b0
GottenZZP <69204281+GottenZZP@users.noreply.github.com>
2021-07-21 17:36:40 +0800

    PAT 乙级题目 -- 1024科学计数法
    此文件为C++版本，可能不是最优解

---
+++ 1024科学计数法.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	string s, stemp;
+	cin >> s;
+	string a, b, c, d;
+	int i = 1;
+	double B, D, result;
+	a = s[0];
+	while (s[i] != 'E')
+	{
+		stemp = s[i++];
+		b.append(stemp);
+	}
+	i++;
+	c = s[i];
+	i++;
+	while (i < (int)s.length())
+	{
+		stemp = s[i++];
+		d.append(stemp);
+	}
+	stringstream geek(b);
+	geek >> B;
+	stringstream aaaa(d);
+	aaaa >> D;
+	result = B * pow(10, D);
+	printf("%c%f", a, result);
+	return 0;
+}
